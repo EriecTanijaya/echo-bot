2a8b72a9dbbf082364a9d113c2bd3cd9da04f3a3
Glitch (eriec-pa) <none>
2020-09-10 02:58:48 +0000

    update deps

--- server.js
+++ server.js
@@ -7,38 +7,36 @@ const express = require("express");
 const app = express();
 
 //isi ini di .env
-//
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET
-}
+};
 
 const client = new line.Client(config);
 
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
+app.post("/callback", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then(result => res.json(result))
+    .catch(err => {
       console.log(err);
-  })
-})
+    });
+});
 
-function handleEvent(event){
+async function handleEvent(event) {
   //cek inputtan user itu typenya harus message dan text
-  if (event.type !== 'message' || event.message.type !== 'text'){
+  if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
-  
-  let text = event.message.text;
-  
-  //api line, cek 
+
+  // buat balasan teks yang sesuai dengan input user
+  const text = event.message.text;
+
+  //api line, cek
   //https://developers.line.biz/en/reference/messaging-api/
   client.replyMessage(event.replyToken, {
-    type: 'text',
+    type: "text",
     text: text
-  })
-  
+  });
 }
 
 // listen for requests :)
